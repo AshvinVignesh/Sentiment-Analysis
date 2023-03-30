28953d48b538d1c1cc9cf1730e5e1ad366a4e4b3
RoshenH <roshen.20210497@iit.ac.lk>
2023-03-30 14:50:55 +0530

    sign up

--- frontend1/flasktest.py
+++ frontend1/flasktest.py
@@ -378,6 +378,32 @@ def login():
 def premium_page():
     return render_template('premium.html')
 
+@app.route('/register', methods =['GET', 'POST'])
+def register():
+    mesage = ''
+    if request.method == 'POST' and 'name' in request.form and 'password' in request.form and 'email' in request.form :
+        userName = request.form['name']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user WHERE email = % s', (email, ))
+        account = cursor.fetchone()
+        if account:
+            mesage = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            mesage = 'Invalid email address !'
+        elif not userName or not password or not email:
+            mesage = 'Please fill out the form !'
+        else:
+            cursor.execute('INSERT INTO user VALUES (NULL, % s, % s, % s)', (userName, email, password, ))
+            mysql.connection.commit()
+            mesage = 'You have successfully registered !'
+            return render_template('loginpage.html', mesage = mesage)
+    elif request.method == 'POST':
+        mesage = 'Please fill out the form !'
+    
+    return render_template('signup.html')
+
 
 
 if __name__ == "__main__":
